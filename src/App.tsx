16f48d3208caf752b41e5b0882b8f718a3d1ccca
CureForCode <artursomov@yahoo.de>
2025-08-27 21:35:02 +0200

    Lesson 10: Group Project. Universities finder by country.

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import GlobalStyles from "styles/GlobalStyles";
 //import Lesson_07 from "lessons/Lesson_07/Lesson_07";
 //import Lesson_08 from "lessons/Lesson_08/Lesson_08";
 //import Lesson_09 from "lessons/Lesson_09/Lesson_09";
+import Lesson_10 from "lessons/Lesson_10/Lesson_10";
 
 // Homeworks
 //import Homework_06 from "homeworks/Homework_06/Homework_06";
 //import Homework_08 from "homeworks/Homework_08/Homework_08";
-import Homework_09 from "homeworks/Homework_09/Homework_09";
+//import Homework_09 from "homeworks/Homework_09/Homework_09";
 
 import "./App.css";
 
@@ -24,9 +25,10 @@ function App() {
       {/* <Lesson_07 /> */}
       {/*<Lesson_08 /> */}
       {/*<Lesson_09 /> */}
+      <Lesson_10 /> 
       {/* Homeworks */}
       {/*<Homework_08 />*/}
-      <Homework_09 />
+      {/* <Homework_09 /> */}
     </div>
   );
 }
